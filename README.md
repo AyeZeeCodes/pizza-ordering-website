4ca8c20416ab71d9a8f14a15f51346780ee68849
ayezee33 <ayezee33@gmail.com>
2016-07-16 08:18:55 -0700

    create a new object, grab form values and pass them to the new object with a constructor. Also has a price prototype for calculating the price of the pizza order.

--- README.md
+++ README.md
@@ -2,6 +2,10 @@
 ###Description
 This is a simple website interface that lets you order a pizza. The goal of this site is to practice using objects, constructors, and prototypes.
 
+###Project requirements
+* Allow the user to choose toppings and size for the pizza they'd like to order.
+* Create a pizza object constructor with properties for toppings and size.
+* Create a prototype method for the cost of a pizza depending on the selections chosen. Use your own formula for this.
 ##Specs
 ###Spec 1
 Expected Input: A user inputs their name
@@ -14,4 +18,4 @@ Expected Input: A user selects toppings
 Expected Output: The toppings are all passed as values of the toppings property
 ###Spec 4
 Expected Input: A user click's the order button
-Expected Output: The object has all the data needed and another action is ready to be performed. 
+Expected Output: The object has all the data needed and another action is ready to be performed.
--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,22 @@
 //Create Pizza Order Object
-//function Order {};
-
-
-
-
-
+function Pizza() {};
+//Creating the order constructor
+function Pizza(pizzaName, pizzaSize, pizzaToppings) {
+  this.name = pizzaName;
+  this.pizzaSize = pizzaSize;
+  this.pizzaToppings = pizzaToppings;
+};
+//Pizza Price Prototype
+Pizza.prototype.price = function() {
+  if (pizzaSize === 'Small') {
+    pizzaPrice = '$14'
+  } else if (pizzaSize === 'Medium') {
+    pizzaPrice = '$18'
+  } else {
+    pizzaPrice = '$22'
+  };
+  console.log("The price for this pizza is " + pizzaPrice);
+}
 
 //Front end, user logic
 $(function() {
@@ -12,4 +24,16 @@ $(function() {
 //  $('#createOrderBtn').click(function() {
 //    $('form#orderForm').fadeIn('slow').show();
 //  });
+  //This empty array allows us to pass a bunch of orders in here.
+  order = [];
+  $('#orderSubmit').click(function(event) {
+  event.preventDefault();
+  pizzaName = $("#name").val();
+  pizzaSize = $("#pizzaSize").val();
+  pizzaToppings = $("input:checkbox:checked").map(function(){
+      return $(this).val();
+    }).get(); //per the docs this turns the jQuery object into a true array. Not sure this is needed.
+  //Let's try and pass all values into a new object with a constructor
+  order[0] = new Pizza(pizzaName, pizzaSize, pizzaToppings);
+  });
 });
