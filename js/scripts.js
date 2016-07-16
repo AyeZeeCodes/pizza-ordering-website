6157394215c80b3653675e3be41eac8c85bdd577
ayezee33 <ayezee33@gmail.com>
2016-07-16 07:30:08 -0700

    Add order form html

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+//Create Pizza Order Object
+//function Order {};
+
+
+
+
+
+
+//Front end, user logic
+$(function() {
+  //Show order form
+//  $('#createOrderBtn').click(function() {
+//    $('form#orderForm').fadeIn('slow').show();
+//  });
+});
